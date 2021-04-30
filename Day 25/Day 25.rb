9c0e737dfd9f8c417ae57ffc24707feddf9e1d28
Simone Castellazzi <simone.caste.95@gmail.com>
2021-04-30 16:43:01 +0200

    Day 1 - Day 2 - Day 3 - Day 25 Part 1

---
+++ Day 25/Day 25.rb
@@ -0,0 +1,14 @@
+MULTIPLIER = 252533
+DIVIDER = 33554393
+STARTING_VALUE = 20151125
+$requested_value = STARTING_VALUE;
+COL = 3019
+ROW = 3010
+INDEX = ((((ROW + COL - 1)**2) + ROW + COL - 1)/2) - (ROW - 1)
+
+for i in 1..INDEX-1
+    $requested_value*=MULTIPLIER
+    $requested_value = $requested_value - (($requested_value/DIVIDER)*DIVIDER)
+end
+
+p $requested_value
